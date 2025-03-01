f6fac96bd44e634be8c4168d0d58f21402d8e8ea
Felipe <felipesantanasantos1812@gmail.com>
2025-03-01 20:52:48 -0300

    feat: Adicionando exibicao do logo e criando arquivo de estilos

---
+++ styles/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    Container:{
+        flex: 1,
+        backgroundColor: '#0097E2',
+        justifyContent: 'center',
+    },
+    Logo:{
+        width: 390,
+        height: 293,
+    }
+})
