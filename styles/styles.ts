fdd594bc1842fb4ed038da09830068527d3d6063
Felipe <felipesantanasantos1812@gmail.com>
2025-03-01 23:32:52 -0300

    feat: Adicionando fonte

--- styles/styles.ts
+++ styles/styles.ts
@@ -9,5 +9,50 @@ export const styles = StyleSheet.create({
     Logo:{
         width: 390,
         height: 293,
+    },
+    ContainerBackground:{
+        flex: 2,
+        backgroundColor: '#FBF7F4',
+        borderTopLeftRadius: 70,
+        
+    },
+    ContainerInput:{
+        marginLeft: 53,
+        backgroundColor: 'red',
+        flex: 1,
+    },
+    ContainerLogo:{
+        flex: 1,
+        backgroundColor: '#0097E2',
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    LogoLogin:{
+        width: 324,
+        height: 243,
+    },
+    ContainerLogin:{
+        flex: 1,
+        backgroundColor: '#0097E2',
+        
+    },
+    LabelEmail:{
+        fontFamily: 'Montserrat-Regular',
+        fontSize: 16,
+        marginTop: 55,
+        marginBottom: 5,
+    },
+    LabelSenha:{
+        fontFamily: 'Montserrat-Regular',
+        fontSize: 16,
+        marginTop: 8,
+        marginBottom: 5,
+    },
+    TextBox:{
+        backgroundColor: '#EDEDED',
+        width: 319,
+        height: 50,
+        borderRadius: 8,
+        paddingHorizontal: 10,
     }
 })
