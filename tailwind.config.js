7695b3131f0cc688f5e598b495f7de976b0eb467
Felipe <felipesantanasantos1812@gmail.com>
2025-03-08 14:57:45 -0300

    feat: importando a font montserrat

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./App.{js,ts,tsx}', './components/**/*.{js,ts,tsx}'],
+  content: ['./App.{js,ts,tsx}', './components/**/*.{js,ts,tsx}', './pages/**/*.{js,jsx,ts,tsx}'],
 
   presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserratRegular: ["MontserratRegular"],
+        montserratMedium: ["MontserratMedium"],
+        montserratSemiBold: ["MontserratSemiBold"],
+        montserratBold: ["MontserratBold"],
+      },
+    },
   },
   plugins: [],
 };
