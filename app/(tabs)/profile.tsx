739a50deea4012466f5e8e4d96aa87a52b04b463
Felipe <felipesantanasantos1812@gmail.com>
2025-03-10 23:33:15 -0300

    feat: Modificando a pagina de profile e adicionando ImagePicker

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,17 +1,41 @@
-import { View, Text, StyleSheet } from 'react-native';
-
+import { View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import { styles } from "styles/styles";
+import Acessar from 'components/Acessar';
+import StyledTextInput from 'components/StyledTextInput';
+import * as ImagePicker from 'expo-image-picker'
+import { useState } from 'react';
+import ImagePickerProfile from 'components/ImagePickerProfile';
 export default function Profile() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Tab [Home|Settings]</Text>
+    <View>
+      <ImagePickerProfile/>
+      <Text className='font-montserratMedium text-2xl pt-5'>Felipe Santana Santos</Text>
+      <View style={styles.ContainerInput}>
+                    <Text className="font-montserratRegular mt-14">
+                        Email
+                    </Text>
+                    <StyledTextInput Iconname="email" placeholder="Digite seu email..." ispassword={false} />
+
+
+                    <Text style={styles.LabelSenha} className="font-montserratRegular">
+                        Senha
+                    </Text>
+                    <StyledTextInput Iconname="lock" placeholder="Digite sua senha..." ispassword={true} />
+
+                    <Acessar tipo="Acessar"/>
+
+                    <View style={{flexDirection: "row",justifyContent: "center", alignItems: "flex-end",flex:1, marginBottom: 50, width: 160,alignSelf: "center"}}>
+                  
+                        <TouchableOpacity>
+                        <Text className="text-base text-[#0097E2] font-montserratRegular">
+                            Politica de privacidade
+                        </Text>
+                    </TouchableOpacity>
+                    </View>
+                </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
+
---
+++ components/ImagePickerProfile.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { View, Button, Image, Alert} from "react-native";
+import * as ImagePicker from "expo-image-picker";
+
+const ImagePickerProfile: React.FC = () => {
+    const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+    const pickImage = async (): Promise<void> => {
+      try {
+        const {status} = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+        if(status != "granted"){
+          Alert.alert("Permissão necessária", "Precisamos da permissão para acessar suas imagens.");
+          return;
+        }
+
+        const result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ["images"],
+          allowsEditing: true,
+          aspect: [4,3],
+          quality: 1
+        });
+
+        if(!result.canceled){
+          setSelectedImage(result.assets[0].uri);
+        }
+      } catch (error) {
+        
+      }
+    }
+    return(
+        <View>
+      <Button title="Selecionar Imagem" onPress={pickImage} />
+      {selectedImage && <Image source={{ uri: selectedImage }}/>}
+    </View>
+    )
+  }
+
+export default ImagePickerProfile
