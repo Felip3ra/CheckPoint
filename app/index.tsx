b31191ac8ec0d9522ac2a94b203cca9fc6b1fc49
Felipe <felipesantanasantos1812@gmail.com>
2025-03-15 23:59:48 -0300

    fix: Corrigindo importacao do montserrat

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,8 @@
 import { Link, Redirect, Stack } from 'expo-router';
 import { View,Text } from 'react-native';
+import { useCustomFonts } from '../hooks/useFonts';
 export default function RootLayout() {
+  const fontsLoaded = useCustomFonts();
   return (
     <Redirect href="(tabs)"/>
   );
